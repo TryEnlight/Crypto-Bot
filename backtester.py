77a96bc1e0fd6f814bfb23cf2cc2ec82ed71ea90
Samay Shamdasani <techboss302@gmail.com>
2018-07-23 22:14:41 -0400

    adding input for investment

--- backtester.py
+++ backtester.py
@@ -2,17 +2,18 @@ import matplotlib.pyplot as plt
 import requests, json
 
 
-def moving_averages(historical_data, ticker):
+def moving_averages(historical_data, ticker, cash):
     """
     If the 3 day average price of ETH is above the 5 day average price, buy. If below, sell.
     """
+    initial = int(cash)
+    cash = int(cash)
     crypto = 0
-    cash = 10000
     x_values = []
     y_values = []
     for place, data_set in enumerate(historical_data[10:-1]):
         three_day_average = get_average([historical_data[place-1], historical_data[place-2], historical_data[place-3]])
-        five_day_average = get_average([historical_data[place-1], 
+        five_day_average = get_average([historical_data[place-1],
                                        historical_data[place-2],
                                        historical_data[place-3],
                                        historical_data[place-4],
@@ -34,7 +35,12 @@ def moving_averages(historical_data, ticker):
         portfolio_value = cash + (crypto * float(data_set["close"]))
         x_values.append(place)
         y_values.append(portfolio_value)
-    print "Final portfolio value:" + str(portfolio_value)
+    print "Final portfolio value: " + str(portfolio_value)
+    net = portfolio_value - initial
+    if (net > 0):
+        print "You profited: " + str(net)
+    else:
+        print("You lost:  ") + str(net)
     plot_graph(x_values, y_values)
 
 
@@ -42,7 +48,7 @@ def get_average(averages_list):
     """
     Gets the average of some numbers
     """
-    total = 0 
+    total = 0
     for data_set in averages_list:
         total += float(data_set["close"])
     return total/len(averages_list)
@@ -62,8 +68,8 @@ def start():
     """
     Here we ask the user for some basic input, fetch our historical data and determine what strategy to use.
     """
-    print "Starting Crypto Bot V1" 
-    ticker = raw_input("Enter ticker:").upper()
+    print "Starting Crypto Bot V1"
+    ticker = raw_input("Enter ticker: ").upper()
     data_url = 'https://min-api.cryptocompare.com/data/histominute?fsym=' + ticker + '&tsym=USD&limit=2000&aggregate=1'
     response = requests.get(data_url)
     try:
@@ -73,8 +79,9 @@ def start():
         quit()
     historical_data = data
     print "Fetched historical data for crypto: " + ticker
-    strategy = raw_input("Select 1) for the moving averages strategy:")
+    cash = raw_input("Enter initial investment: ")
+    strategy = raw_input("Select (1) for the moving averages strategy: ")
     if strategy == "1":
-        moving_averages(historical_data, ticker)
+        moving_averages(historical_data, ticker, cash)
 
 start()
